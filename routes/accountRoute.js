2d15b87f5ea5e6508bfdaa31a81d2fb252ea7e93
Lilly Armenta <larmenta@byupathway.edu>
2025-09-22 22:31:12 -0600

    W04 Learning Activities: Inserting and Validating Data

---
+++ routes/accountRoute.js
@@ -0,0 +1,31 @@
+// Needed Resources
+const express = require("express")
+const router = new express.Router()
+const utilities = require("../utilities")
+const accountsController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation');
+
+
+// Route to build login view
+router.get(
+  "/login",
+  utilities.handleErrors(accountsController.buildLogin)
+)
+
+// Route to build registration view
+router.get(
+    "/register", 
+    utilities.handleErrors(accountsController.buildRegister)
+)
+
+// Route to process registration form
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountsController.registerAccount)
+)
+
+
+module.exports = router
+
--- server.js
+++ server.js
@@ -13,6 +13,35 @@ const expressLayouts = require("express-ejs-layouts")
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 /* ***********************
@@ -30,6 +59,10 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// Account routes 
+app.use("/account", accountRoute)
+
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
@@ -64,3 +97,6 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+
+
